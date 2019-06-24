b63d3c572df84782a14033e260f1ed2ffcf45ce5
sh4869 <nobuk4869@gmail.com>
2019-06-24 19:13:32 +0900

    とりあえず間違ってるけど作った。木構造を頑張ってほしい。

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,25 @@
+interface Dependency {
+    name: string;
+    // TODO: 複雑な依存条件の設定が可能にする
+    version: string;
+    depndencies: Dependency[];
+}
 
+interface DependencyGraphNode {
+    parent: Symbol;
+    self: Symbol;
+    // 内容
+    dependency: Dependency;
+    // 深さ
+    depth: number;
+}
+
+interface DependencyGraph {
+    // package.jsonに明記されているDependenciesを書く。
+    dependencies: DependencyGraphNode[]; 
+}
+
+const getAllDepndencies = (dependencies: Dependency[]) => {
+}
 
 console.log("hello,world")
