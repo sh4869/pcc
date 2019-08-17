69cd7398ac62b36ff3da99e2b163e4b0f92c831c
sh4869 <nobuk4869@gmail.com>
2019-08-17 18:50:46 +0900

    依存関係の重複あたりまではわかってきた

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import logicTree = require("npm-logical-tree");
 
 import util = require("util");
 import semver, { SemVer } from "semver";
-import { PkgDataInfo, PackageDependenciesInfo, Dependencies } from "./type";
+import {
+  PkgDataInfo,
+  PackageDependenciesInfo,
+  Dependencies,
+  LogicalTree,
+  SimplePackageInfo,
+  ConflictInfo,
+  DependencyRootInfo
+} from "./type";
 import packagejson from "package-json";
 import packageJson = require("package-json");
+import { readFileSync } from "fs";
+import { createHash } from "crypto";
 
 // npm-registory-package-infoを使ってPkgDataInfoを取得する
 const getPackageInfo = async (opts: pkginfo.Options) => {
@@ -17,6 +27,7 @@ const getPackageInfo = async (opts: pkginfo.Options) => {
 
 /**
  * 与えられたパッケージの依存関係を返す
+ * TODO: キャッシュを残す
  * @param packages パッケージ名の配列
  */
 const getPackageDependencies = async (packages: string[]): Promise<Map<string, PackageDependenciesInfo>> => {
@@ -37,11 +48,122 @@ const getPackageDependencies = async (packages: string[]): Promise<Map<string, P
   return map;
 };
 
-const getPackageJsonInfo = async (path: string): Promise<packageJson.FullVersion> => {
-  const readJosnPromise = util.promisify(readJson);
-  return (await readJosnPromise(path)) as packagejson.FullVersion;
+const getJsondata = (path: string): any => {
+  const result = readFileSync(path).toString();
+  return JSON.parse(result);
 };
 
-getPackageJsonInfo("package.json").then(v => console.log(v));
+const getLogicTree = () => {
+  return logicTree(getJsondata("package.json"), getJsondata("package-lock.json")) as LogicalTree;
+};
 
-//getPackageDependencies(["npm-api"]).then(v => console.log(v));
+/**
+ * 依存関係のtreeを取り除く
+ * @param logicalTree 依存関係のtree
+ */
+const getRealLogicalTree = (logicalTree: LogicalTree): Map<string, LogicalTree> => {
+  const depens = logicalTree.dependencies;
+  // optional と devなdependenciesを除く
+  const realDep = new Map<string, LogicalTree>();
+  depens.forEach((target, x) => {
+    if (target && (!target.dev && !target.optional && !target.bundled)) {
+      realDep.set(x, target);
+    }
+  });
+  return realDep;
+};
+
+const getConfilct = (dependencis: Map<string, LogicalTree>): ConflictInfo => {
+  let deps: ConflictInfo = {};
+  let check = new Set<string>();
+  const addVersion = (logicalTree: LogicalTree, parentArray: SimplePackageInfo[], bigParent: SimplePackageInfo) => {
+    const key =
+      Buffer.from(logicalTree.name, "utf-8").toString("base64") +
+      Buffer.from(logicalTree.version, "utf-8").toString("base64");
+    if (!check.has(key)) {
+      check.add(key);
+      if (!deps[logicalTree.name]) {
+        deps[logicalTree.name] = [{ name: logicalTree.name, version: logicalTree.version, parentArray, bigParent }];
+      } else {
+        deps[logicalTree.name].push({ name: logicalTree.name, version: logicalTree.version, parentArray, bigParent });
+      }
+      parentArray.push({ name: logicalTree.name, version: logicalTree.version });
+      logicalTree.dependencies.forEach(v => {
+        const newBigParent =
+          bigParent.name === "#ROOT" ? { name: logicalTree.name, version: logicalTree.version } : bigParent;
+        addVersion(v, parentArray, newBigParent);
+      });
+    }
+  };
+  dependencis.forEach(v => addVersion(v, [], { name: "#ROOT", version: "" }));
+  for (let i in deps) {
+    if (deps[i].length == 1) {
+      delete deps[i];
+      continue;
+    }
+    const firstVersion = deps[i][0].version;
+    if (deps[i].every(v => v.version === firstVersion)) {
+      delete deps[i];
+    }
+  }
+  return deps;
+};
+
+const getSolvableConflicts = (conflictinfo: ConflictInfo): ConflictInfo => {
+  const solvableConfilts = {};
+  for (let x in conflictinfo) {
+    const depends = conflictinfo[x];
+    // 依存の衝突の大元が同じパッケージの場合ユーザーには解決出来ない
+    const bigParents = depends.map(d => d.bigParent.name);
+    const uniqueBigParents = bigParents.filter((e, i, s) => s.indexOf(e) === i);
+    if (bigParents.length === uniqueBigParents.length) {
+      solvableConfilts[x] = conflictinfo[x];
+    }
+  }
+  return solvableConfilts;
+};
+
+const searchNonConfilictVersion = async (
+  name: string,
+  dependencyRootInfo: DependencyRootInfo[],
+  allowDowngrade: boolean = false
+) => {
+  let result = false;
+  for (let x in dependencyRootInfo) {
+    // プロジェクトから直接参照されている場合はそのパッケージのバージョンの候補を探す
+    // プロジェクト以外から参照されている場合はそのパッケージの一番親のパッケージのバージョンの候補を探す
+    const requiredpackage =
+      dependencyRootInfo[x].bigParent.name === "#ROOT"
+        ? { name: dependencyRootInfo[x].name, version: dependencyRootInfo[x].version }
+        : dependencyRootInfo[x].bigParent;
+    const packageVersions = (await getPackageDependencies([requiredpackage.name])).get(requiredpackage.name);
+    const currentVersion = semver.parse(requiredpackage.version);
+    if (!packageVersions || !currentVersion) {
+      throw new Error("Package info does not found");
+    }
+    const toCheckVersion = allowDowngrade
+      ? Array.from(packageVersions.keys())
+      : Array.from(packageVersions.keys()).filter(v => semver.gt(v, currentVersion));
+    if (toCheckVersion.length === 0) {
+      continue;
+    }
+    toCheckVersion.forEach(v => {
+      const dependencyInfo = packageVersions.get(v);
+      if (dependencyInfo !== undefined) {
+        dependencyInfo;
+      }
+    });
+  }
+  console.log(result);
+};
+
+const solvableConfilts = getSolvableConflicts(getConfilct(getRealLogicalTree(getLogicTree())));
+
+// console.log(solvableConfilts);
+//console.log(util.inspect(solvableConfilts, false, null));
+
+for (let x in solvableConfilts) {
+  searchNonConfilictVersion(x, solvableConfilts[x]);
+}
+
+//console.log(util.inspect(getSolvableConflicts(getConfilct(getRealLogicalTree(getLogicTree()))), false, null));
--- src/type.ts
+++ src/type.ts
@@ -16,3 +16,33 @@ export type Dependencies = { [name: string]: string };
  * あるパッケージにおけるバージョンとそのバージョンの依存関係
  */
 export type PackageDependenciesInfo = Map<SemVer, Dependencies>;
+
+export type SimplePackageInfo = { name: string; version: string };
+
+/**
+ * あるパッケージのあるバージョンがどういう依存関係によって存在しているか
+ */
+export type DependencyRootInfo = {
+  name: string;
+  version: string;
+  parentArray: SimplePackageInfo[];
+  bigParent: SimplePackageInfo;
+};
+
+/**
+ * 依存関係が重複しているパッケージの情報
+ */
+export type ConflictInfo = { [name: string]: DependencyRootInfo[] };
+
+export interface LogicalTree {
+  name: string;
+  version: string;
+  address: string;
+  optional: boolean;
+  dev: boolean;
+  bundled: boolean;
+  resolved: any;
+  integrity: any;
+  dependencies: Map<string, LogicalTree>;
+  requiredBy?: Set<LogicalTree>;
+}
