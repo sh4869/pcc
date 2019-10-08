b4ccf96b52fc89098b06b25ef79d4572b716338c
sh4869 <nobuk4869@gmail.com>
2019-10-08 17:37:38 +0900

    delete unused code

--- src/type.ts
+++ src/type.ts
@@ -12,37 +12,6 @@ export type Dependencies = { [name: string]: DependecyRange };
  */
 export type PackageDependenciesInfo = Map<SemVer, Dependencies>;
 
-export type SimplePackageInfo = { name: string; version: string };
-
-/**
- * パッケージの依存関係リスト
- */
-export interface PackageDepndecyList {
-  package: SimplePackageInfo;
-  depndecies: Set<SimplePackageInfo>;
-}
-
-/**
- * あるパッケージのあるバージョンがどういう依存関係によって存在しているか
- */
-export type DependencyRootInfo = {
-  name: string;
-  version: string;
-  parentArray: SimplePackageInfo[];
-  bigParent: SimplePackageInfo;
-};
-
-/**
- * 依存関係が重複しているパッケージの情報
- */
-export type ConflictInfo = { [name: string]: DependencyRootInfo[] };
-
-export interface ConflictPackageInfo {
-  name: string;
-  conflictVersions: string[];
-  conflictInfo: DependencyRootInfo[];
-}
-
 export interface LogicalTree {
   name: string;
   version: string;
