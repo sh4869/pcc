76dd1604eb5496973a160f517bedffe727b7613b
sh4869 <nobuk4869@gmail.com>
2019-10-09 14:28:40 +0900

    fix display

--- src/cli.ts
+++ src/cli.ts
@@ -4,19 +4,33 @@ import { getLogicTree } from "./logic_tree";
 import { NpmConflictSolver } from "./conflict_solver/npm_conflict_solver";
 import { NpmPackageRepository } from "./get_package_info";
 import { ConflictPackage, Package } from "./type";
-import { version } from "punycode";
-import { Z_ASCII } from "zlib";
+import chalk = require("chalk");
 
 const pcc = new Command("pcc");
 
 const printConflitResult = (conflictResult: ConflictPackage[]): void => {
+  console.log(`find ${conflictResult.length} conflicts package`);
   conflictResult.forEach(pack => {
     const groupByVersions: { [key: string]: Package[][] } = {};
     pack.versions.forEach(v => {
       if (groupByVersions[v.version.version]) groupByVersions[v.version.version].push(v.depenedecyRoot);
       else groupByVersions[v.version.version] = [v.depenedecyRoot];
     });
-    console.log(`${pack.packageName}`);
+    console.log(chalk.default.green(`- ${pack.packageName}`));
+    for (const ver in groupByVersions) {
+      console.log(`    ${chalk.default.red(ver)}`);
+      groupByVersions[ver].forEach(packages => {
+        const space = 4;
+        packages.forEach((pac, i) => {
+          const prefix = `${" ".repeat(space + i * 2)}${i === 0 ? "  " : "+-"}`;
+          const func =
+            pac.name === pack.packageName && pac.version.version === ver
+              ? chalk.default.bold
+              : (str: string): string => str;
+          console.log(prefix + func(pac.name + "@" + pac.version));
+        });
+      });
+    }
   });
 };
 
@@ -27,7 +41,7 @@ pcc
   .description("check conflict in package")
   .action((dir, _) => {
     const result = new NpmConflictChecker().checkConflict(getLogicTree(dir as string));
-    console.log(result);
+    printConflitResult(result);
   });
 
 pcc
