52381fb9bfc81add4bfb15d3f7395657aac2194f
sh4869 <nobuk4869@gmail.com>
2019-10-24 04:38:31 +0900

    remove unused functions, fix cache in api, catch error in request

--- src/get_package_info.ts
+++ src/get_package_info.ts
@@ -11,54 +11,9 @@ interface PkgData {
   versions: { [key: string]: packagejson.AbbreviatedVersion };
 }
 
+const sleep: (number: number) => Promise<void> = msec => new Promise(resolve => setTimeout(resolve, msec));
+
 type PkgDataInfo = { [key: string]: PkgData };
-
-// キャッシュを持つ
-const cache: PkgDataInfo = {};
-
-// npm-registory-package-infoを使ってPkgDataInfoを取得する
-const getPackageInfo = async (opts: pkginfo.Options): Promise<PkgDataInfo> => {
-  const newOpts = {
-    packages: opts.packages.filter(v => !Object.keys(cache).includes(v))
-  };
-  if (newOpts.packages.length > 0) {
-    const pkginfoPromise = util.promisify(pkginfo);
-    const newData = (await pkginfoPromise(newOpts)).data as PkgDataInfo;
-    // キャッシュを保存
-    for (const i in newData) {
-      cache[i] = newData[i];
-    }
-  }
-  const result = {};
-  // キャッシュからデータを取り出し
-  // 非効率的だけど大した数にはならないので
-  opts.packages.forEach(v => {
-    result[v] = cache[v];
-  });
-  return result as PkgDataInfo;
-};
-
-/**
- * 与えられたパッケージの依存関係を返す
- * @param packages パッケージ名の配列
- */
-export const getPackageDependencies = async (packages: string[]): Promise<Map<string, PackageDependenciesInfo>> => {
-  const pkgdatainfo = await getPackageInfo({ packages: packages });
-  const map = new Map<string, PackageDependenciesInfo>();
-  for (const name in pkgdatainfo) {
-    const xmap = new Map<SemVer, Dependencies>();
-    for (const ver in pkgdatainfo[name].versions) {
-      const version = semver.parse(ver);
-      if (version) {
-        xmap.set(version, pkgdatainfo[name].versions[ver].dependencies || {});
-      } else {
-        throw new Error(`Semantic Version Parse Error: ${name} - ${ver}`);
-      }
-    }
-    map.set(name, xmap);
-  }
-  return map;
-};
 
 /**
  * Npm PackageRepository Implements
@@ -71,14 +26,20 @@ export class NpmPackageRepository implements PackageRepository {
 
   private async fetchPackageInfo(opts: pkginfo.Options): Promise<PkgDataInfo> {
     const newOpts = {
-      packages: opts.packages.filter(v => !Object.keys(cache).includes(v))
+      packages: opts.packages.filter(v => !Object.keys(this.cache).includes(v))
     };
     if (newOpts.packages.length > 0) {
-      const pkginfoPromise = util.promisify(pkginfo);
-      const newData = (await pkginfoPromise(newOpts)).data as PkgDataInfo;
-      // キャッシュを保存
-      for (const i in newData) {
-        this.cache[i] = newData[i];
+      try {
+        const pkginfoPromise = util.promisify(pkginfo);
+        const newData = (await pkginfoPromise(newOpts)).data as PkgDataInfo;
+        // キャッシュを保存
+        for (const i in newData) {
+          this.cache[i] = newData[i];
+        }
+      } catch {
+        // 1秒待ってからやり直す
+        await sleep(1000);
+        return this.fetchPackageInfo(opts);
       }
     }
     const result = {};
