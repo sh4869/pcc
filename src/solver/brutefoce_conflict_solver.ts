410d42707c512f3c45d26ef0b381f2d5162ad64f
sh4869 <nobuk4869@gmail.com>
2019-11-29 18:34:24 +0900

    fix speed of get package info

--- src/solver/brutefoce_conflict_solver.ts
+++ src/solver/brutefoce_conflict_solver.ts
@@ -1,10 +1,10 @@
-import { ConflictSolver, NoConflictSituation, Package, PackageUpdateInfo } from "../misc/type";
+import { ConflictSolver, NoConflictSituation, Package, PackageUpdateInfo, Dependencies } from "../misc/type";
 import semver, { SemVer } from "semver";
 import { PackageRepository } from "../misc/npm/package_repository";
 
 type PackageDepndecyList = {
   package: Package;
-  depndecies: Set<Package>;
+  depndecies: Array<Package>;
 };
 
 const getValidLatestVersion = (condition: string, versions: semver.SemVer[]): semver.SemVer => {
@@ -17,43 +17,51 @@ export class BruteforceConflictSolver implements ConflictSolver {
     this.packageRepository = repository;
   }
 
-  private async getDependencies(pack: Package): Promise<PackageDepndecyList> {
-    const dependecyList: Set<Package> = new Set();
-    const dependecyMap: Map<string, Package> = new Map();
-    const addDependency = async (
-      packageInfo: Package,
-      dependecy: Map<semver.SemVer, { [name: string]: string }>
-    ): Promise<void> => {
-      if (!dependecyMap.has(JSON.stringify(packageInfo))) {
-        dependecyMap.set(JSON.stringify(packageInfo), packageInfo);
-        dependecyList.add(packageInfo);
-        // 当てはまるバージョンの依存関係を取得する(Objectなので適当にgetすると失敗するため)
-        const verisonDependecy = dependecy.get(
-          Array.from(dependecy.keys()).filter(v => v.version === packageInfo.version.version)[0]
-        );
-        if (verisonDependecy) {
-          const dependecyNames = Array.from(Object.keys(verisonDependecy));
-          const packageDependecyInfo = await this.packageRepository.getMultiDependencies(dependecyNames);
-          for (const name in verisonDependecy) {
-            const versions = packageDependecyInfo.get(name);
+  private async getDependenciesS(
+    pack: Package,
+    cache: Map<string, Package>,
+    dep: Dependencies | undefined
+  ): Promise<Array<Package>> {
+    if (!cache.has(JSON.stringify(pack))) {
+      cache.set(JSON.stringify(pack), pack);
+      if (dep) {
+        const dependecyNames = Array.from(Object.keys(dep));
+        return this.packageRepository.getMultiDependencies(dependecyNames).then(async v => {
+          const promieses: Promise<Array<Package>>[] = [];
+          const x: Package[] = [];
+          for (const name in dep) {
+            const versions = v.get(name);
             if (versions) {
-              const targetVersion = getValidLatestVersion(verisonDependecy[name], Array.from(versions.keys()));
-              await addDependency({ name: name, version: targetVersion }, versions);
+              const targetVersion = getValidLatestVersion(dep[name], Array.from(versions.keys()));
+              x.push({ name: name, version: targetVersion });
+              promieses.push(
+                this.getDependenciesS({ name: name, version: targetVersion }, cache, versions.get(targetVersion))
+              );
             }
           }
-        }
+          const d = await Promise.all(promieses);
+          return d.reduce((r, l) => r.concat(l), []).concat(x);
+        });
+      } else {
+        return [];
       }
-    };
-    await addDependency(pack, await this.packageRepository.getDependencies(pack.name));
-    return { package: pack, depndecies: dependecyList };
+    } else {
+      return [];
+    }
+  }
+
+  private async getDependencies(pack: Package): Promise<Array<Package>> {
+    const x = await this.packageRepository.getDependencies(pack.name);
+    if (!x) throw new Error("x");
+    const dep = x.get(Array.from(x.keys()).filter(v => semver.eq(v, pack.version))[0]);
+    return [...(await this.getDependenciesS(pack, new Map(), dep)), pack];
   }
 
   private isSolvedConfilicts(targets: string[], gathering: PackageDepndecyList[]): false | Package[] {
-    //const versions: Array<SemVer[]> = new Array(targets.length);
     const packs: Package[] = [];
     gathering.forEach(v => {
       packs.push(v.package);
-      Array.from(v.depndecies.values()).forEach(d => {
+      v.depndecies.forEach(d => {
         if (targets.includes(d.name)) {
           packs.push(d);
         }
@@ -84,7 +92,10 @@ export class BruteforceConflictSolver implements ConflictSolver {
     const packageDepndencyMap: {
       [name: string]: Map<SemVer, PackageDepndecyList>;
     } = {};
+    const cache: string[] = [];
     for (const targetPackage of conflictCausePackages) {
+      if (cache.includes(targetPackage.name)) continue;
+      cache.push(targetPackage.name);
       // どうアップデートするべきかを表示するために必要
       beforeVersions[targetPackage.name] = targetPackage;
       const packageVersions = await this.packageRepository.getDependencies(targetPackage.name);
@@ -92,11 +103,10 @@ export class BruteforceConflictSolver implements ConflictSolver {
       const versionDependecyMap = new Map<semver.SemVer, PackageDepndecyList>();
       for (const v of checkVersions) {
         const depndecyList = await this.getDependencies({ name: targetPackage.name, version: v });
-        versionDependecyMap.set(v, depndecyList);
+        versionDependecyMap.set(v, { package: { name: targetPackage.name, version: v }, depndecies: depndecyList });
       }
       packageDepndencyMap[targetPackage.name] = versionDependecyMap;
     }
-
     const conflictCauseNames: Array<string> = Array.from(Object.keys(packageDepndencyMap));
     const noConflictSituation: NoConflictSituation[] = [];
     const checkVersion = (
